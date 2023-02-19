bec6a03ef6ff3e4d0f067385c04301f4e64e52da
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-19 14:49:25 -0500

    fixed code in dash

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,29 +1,26 @@
 const createBlogHandler = async (event) => {
     try {
-       //  prevent page from loading
-    event.preventDefault();
-   
-    // gather user email and password inputs
-    const title = $('#blog-title').val().trim();
-    const description = $('#blog-desc').val().trim();
+      // prevent page from loading
+      event.preventDefault();
   
-    if (title && description) {
-      //  goto end-point to create new blog
-      const response = await fetch('/api/blogs', {
-        method: 'POST',
-        body: JSON.stringify({ title, description,  }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+      // gather user email and password inputs
+      const title = $('#blog-title').val().trim();
+      const description = $('#blog-desc').val().trim();
   
-      
-      if (response.ok) {
-        window.location.href = '/dash'}
-      } else {
-       res.status(404).json({ message: 'Blog not created!' });
+      if (title && description) {
+        // goto end-point to create new blog
+        const response = await fetch('/api/blogs', {
+          method: 'POST',
+          body: JSON.stringify({ title, description }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          window.location.href = '/dash';
+        } else {
+          res.status(404).json({ message: 'Blog not created!' });
+        }
       }
-      
-  
-    }
     } catch (error) {
       res.status(400).json(err);
     }
@@ -32,48 +29,48 @@ const createBlogHandler = async (event) => {
   
   const expandBlogHandler = async (event) => {
     try {
-       //  prevent page from loading
-    event.preventDefault();
-   
-    // gather user email and password inputs
-    const blog_id = $('#single-blog').href('id');
-    console.log(blog_id);
-    
-    if (blog_id) {
-      //  goto end-point to create new blog
-      const response = await fetch(`/blogpage/${blog_id}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      });
+      // prevent page from loading
+      event.preventDefault();
   
-    }
+      // gather user email and password inputs
+      const blog_id = $('#single-blog').href('id');
+      console.log(blog_id);
+  
+      if (blog_id) {
+        // goto end-point to create new blog
+        const response = await fetch(`/blogpage/${blog_id}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+      }
     } catch (error) {
       res.status(400).json(err);
     }
   };
   
   const deleteBlogHandler = async (event) => {
-    console.log("Clicked  DELETE Buttom")
+    console.log("Clicked DELETE Button");
     try {
-       //  prevent page from loading
-    event.preventDefault();
-   
-    // retrieve the data id from the button
-    const delete_id = $('#delete-blog-btn').data('id');
-    console.log(delete_id);
-    
-    if (delete_id) {
-      //  goto end-point to delete blog
-      const response = await fetch(`/api/blogs/${delete_id}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-      });
+      // prevent page from loading
+      event.preventDefault();
   
-      if (response.ok) {
-       window.location.href = '/dash'}
-     } else {
-      res.status(404).json({ message: 'No blog found with this id!' });
-     }
+      // retrieve the data id from the button
+      const delete_id = $('#delete-blog-btn').data('id');
+      console.log(delete_id);
+  
+      if (delete_id) {
+        // goto end-point to delete blog
+        const response = await fetch(`/api/blogs/${delete_id}`, {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+          window.location.href = '/dash';
+        } else {
+          res.status(404).json({ message: 'No blog found with this id!' });
+        }
+      }
     } catch (error) {
       res.status(400).json(err);
     }
@@ -83,3 +80,4 @@ const createBlogHandler = async (event) => {
   $('#create-blog-btn').on('click', createBlogHandler);
   $('#delete-blog-btn').on('click', deleteBlogHandler);
   $('#single-blog').on('click', expandBlogHandler);
+  
